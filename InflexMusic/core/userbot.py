c3974dff2b0281450d92c3bd8f51f074f76f9082
Anku <158991451+Ankit21ri@users.noreply.github.com>
2024-06-05 16:37:23 +0530

    Update userbot.py

--- InflexMusic/core/userbot.py
+++ InflexMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexSupport")
+                await self.one.join_chat("omXmusic")
+                await self.one.join_chat("om_support_grp")
             except:
                 pass
             assistants.append(1)
